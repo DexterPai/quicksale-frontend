a606d4c81cf31d851f07b923c187e8728dd47eb0
DexterPai <rrchk.22@gmail.com>
2025-06-29 22:52:13 -0500

    quicksale-base

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    projectId: 'quicksale-dev',
+    appId: '1:438610308770:web:9a5680a492857641fdf1fb',
+    storageBucket: 'quicksale-dev.firebasestorage.app',
+    apiKey: 'AIzaSyBsLKOYG5LLslCp0Nrm_3doWo5iM7NxZZA',
+    authDomain: 'quicksale-dev.firebaseapp.com',
+    messagingSenderId: '438610308770',
+    measurementId: 'G-JEQ0DSJMXM',
+  },
+  production: false,
+  host: 'http://localhost:8080/api'
+  //host:'https://api-quicksale-ci-a4801eee611d.herokuapp.com/api'
+};
